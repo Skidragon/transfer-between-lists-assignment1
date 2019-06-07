24ff101ebaf57ca45bd52e1d88db701b08f94883
Simon Davis <simon_freelance@hotmail.com>
2019-06-07 19:27:53 -0400

    Initial setup

---
+++ src/components/ListsTransferer/ListsTransfererContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ListsTransfererContainer extends Component {
+  state = {
+    list1: [],
+    list2: [],
+  };
+
+  render() {
+    return <div />;
+  }
+}
+
+export { ListsTransfererContainer };
---
+++ src/views/home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { ListsTransfererContainer } from "../../components/ListsTransferer/ListsTransfererContainer";
+const Home = () => {
+  return <ListsTransfererContainer />;
+};
+
+export default Home;
