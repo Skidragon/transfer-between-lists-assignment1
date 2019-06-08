256e87261c1a80683262a5e9a0038ed948ad7c81
Simon Davis <simon_freelance@hotmail.com>
2019-06-07 21:58:30 -0400

    Added isChecked prop to mock-lists data

--- src/components/ListsTransferer/lib/mock-data/mock-lists.js
+++ src/components/ListsTransferer/lib/mock-data/mock-lists.js
@@ -2,14 +2,17 @@ const mockList1 = [
   {
     id: 1,
     name: "sushi",
+    isChecked: false,
   },
   {
     id: 2,
     name: "bananas",
+    isChecked: false,
   },
   {
     id: 3,
     name: "ice cream",
+    isChecked: false,
   },
 ];
 
@@ -17,14 +20,17 @@ const mockList2 = [
   {
     id: 4,
     name: "a",
+    isChecked: false,
   },
   {
     id: 5,
     name: "b",
+    isChecked: false,
   },
   {
     id: 6,
     name: "c",
+    isChecked: false,
   },
 ];
 
