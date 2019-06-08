d629b82366403fe368facef9c45a41b0b7196373
Simon Davis <simon_freelance@hotmail.com>
2019-06-07 20:44:01 -0400

    Made mock data

---
+++ src/components/ListsTransferer/lib/mock-data/mock-lists.js
@@ -0,0 +1,31 @@
+const mockList1 = [
+  {
+    id: 1,
+    name: "sushi",
+  },
+  {
+    id: 2,
+    name: "bananas",
+  },
+  {
+    id: 3,
+    name: "ice cream",
+  },
+];
+
+const mockList2 = [
+  {
+    id: 4,
+    name: "a",
+  },
+  {
+    id: 5,
+    name: "b",
+  },
+  {
+    id: 6,
+    name: "c",
+  },
+];
+
+export { mockList1, mockList2 };
